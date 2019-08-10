ff538b520c2f41cf985059d6717835ca93aa4ad6
James <yangj_2003@hotmail.com>
2019-08-10 16:33:23 +0800

    Add Kosaraju scc search algorithm

--- python_alg/scc.py
+++ python_alg/scc.py
@@ -1,4 +1,4 @@
-
+from utils import dfs_topsort, walk
 
 def find_sccs(G, s):
     """
@@ -59,26 +59,73 @@ def find_sccs_rec(G, s, cur_path=[], sccs=[]):
     cur_path.pop()
 
     return sccs
+    
+# below algorithm is from the book
+# step1. transpose all edges of G
+# step2. top sort G
+# step3. traverse GT
+
+def tr(G):
+    GT = {}
+    for v in G:
+        GT[v] = set()
+        
+    for v in G:
+        for u in G[v]:
+            GT[u].add(v)
+            
+    return GT
+    
+def find_scc(G):
+    GT = tr(G)
+    sccs, seen = [], set()
+    res = dfs_topsort(G)
+    for u in res:
+        if u in seen:
+            continue
+        C = walk(GT, u, seen)
+        seen.update(C)
+        sccs.append(C)
+        
+    return sccs
+
+
+        
 
 
 if __name__ == '__main__':
     G = {
-        'a': ['b', 'c'],
-        'b': ['e', 'd'],
-        'd': ['a'],
-        'c': ['d'],
-        'e': ['f'],
-        'f': ['g'],
-        'g': ['e', 'h'],
-        'h': ['i'],
-        'i': ['h', 'j'],
-        'j':[]
+        'a': set(['b', 'c']),
+        'b': set(['e', 'd']),
+        'd': set(['a']),
+        'c': set(['d']),
+        'e': set(['f']),
+        'f': set(['g']),
+        'g': set(['e', 'h']),
+        'h': set(['i']),
+        'i': set(['h', 'j']),
+        'j':set([])
 
     }
-
 
     for n in G:
         sccs = []
         print('start node: %s' % n)
         sccs = find_sccs_rec(G, n, cur_path=[], sccs=[])
         print(sccs)
+        
+    resultSet = find_scc(G)
+   
+    for ret in resultSet:
+        print ret.keys()
+        
+        
+    res = dfs_topsort(G)
+    
+    print("top sort result:\n%s" % str(res))
+    res.reverse()
+    print("reverse of top sort result:\n%s" % str(res))
+    
+        
+        
+    
